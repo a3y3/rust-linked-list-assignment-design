0287a10b74ea860271e084ae8dcd0c63edd1b75a
a3y3 <sohamssd@gmail.com>
2021-03-17 11:42:54 -0400

    Init with structure of assignment

---
+++ soln/hashmap/src/main.rs
@@ -0,0 +1,4 @@
+use linkedlist::LinkedList;
+
+fn main() {
+}
---
+++ soln/linkedlist/src/lib.rs
@@ -0,0 +1,9 @@
+pub struct LinkedList {}
+impl LinkedList {
+    pub fn new() -> LinkedList {
+        LinkedList {}
+    }
+}
+
+
+mod tests;
