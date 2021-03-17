775cb124324d6de6e48378760b51d7f0677081fe
a3y3 <sohamssd@gmail.com>
2021-03-17 12:57:57 -0400

    Add tests for push_back

--- soln/linkedlist/src/lib.rs
+++ soln/linkedlist/src/lib.rs
@@ -1,9 +1,28 @@
-pub struct LinkedList {}
-impl LinkedList {
-    pub fn new() -> LinkedList {
-        LinkedList {}
-    }
+pub struct LinkedList<T> {
+    _bogus: T,
 }
+impl<T> LinkedList<T> {
+    pub fn new(value: T) -> LinkedList<T> {
+        unimplemented!()
+    }
 
+    pub fn size(&self) -> usize {
+        unimplemented!()
+    }
+
+    pub fn push_back(&mut self, value: T) {
+        unimplemented!()
+    }
+
+    pub fn peek_front(&self) -> &T {
+        unimplemented!()
+    }
+    
+    pub fn peek_back(&self) -> &T {
+        unimplemented!()
+    }
+
+    pub fn iter() {}
+}
 
 mod tests;
--- soln/linkedlist/src/tests.rs
+++ soln/linkedlist/src/tests.rs
@@ -1,7 +1,42 @@
 #[cfg(test)]
 mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    use super::super::LinkedList;
+    struct Holder<T> {
+        value: T,
     }
+    const values: [&str; 8] = [
+        "apple",
+        "banana",
+        "grape",
+        "jackfruit",
+        "pineapple",
+        "cantaloupe",
+        "",
+        "melon",
+    ];
+
+    //push_front
+
+
+    // push_back
+    fn test_push_back() {
+        let mut linked_list = LinkedList::new(Holder { value: values[0] });
+        let mut correct_size = 1;
+        assert_eq!(correct_size, linked_list.size(), "Size mismatch!");
+        let last_val = linked_list.peek_back();
+        assert_eq!(values[0], last_val.value, "Incorrect value peeked");
+
+        for value in &values {
+            let holder = Holder { value: *value };
+            linked_list.push_back(holder);
+            correct_size += 1;
+            assert_eq!(correct_size, linked_list.size(), "Size mismatch!");
+            let last_val = linked_list.peek_back();
+            assert_eq!(value, last_val.value, "Incorrect value peeked");
+        }
+    }
+
+    // find
+    //pop_front
+    // pop_back
 }
